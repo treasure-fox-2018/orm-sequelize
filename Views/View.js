33cb030ce4ca4705bd259780ba37c4a36362b1f0
dawimuhammad <muhamadhaddawi@gmail.com>
2018-06-28 22:21:44 +0700

    Release 1 - all crud for author table

--- Views/View.js
+++ Views/View.js
@@ -3,9 +3,21 @@
 class View {
     static showHelp() {
         console.log('=====  documentation  ==================================')
-        console.log('author add       ->  add<space> "data yang ingin dimasukan"')
-        console.log('author read_one  ->  read_one<space> "masukkan id author"')
-        console.log('author read_all  ->  read_all')
+        console.log('author add        ->  add<space> "data yang ingin dimasukan"')
+        console.log('author read_one   ->  read_one<space> "masukkan id author"')
+        console.log('author read_all   ->  read_all')
+        console.log('author update     ->  update<space> "masukkan data yang ingin di update dan idnya"')
+        console.log('author delete     ->  delete<space> "masukkan id author"')
+        console.log('tag add           ->  add<space> "data yang ingin dimasukan"')
+        console.log('tag read_one      ->  read_one<space> "masukkan id tag"')
+        console.log('tag read_all      ->  read_all')
+        console.log('tag update        ->  update<space> "masukkan data yang ingin di update dan idnya"')
+        console.log('tag delete        ->  delete<space> "masukkan id tag"')
+        console.log('article add       ->  add<space> "data yang ingin dimasukan"')
+        console.log('article read_one  ->  read_one<space> "masukkan id article"')
+        console.log('article read_all  ->  read_all')
+        console.log('article update    ->  update<space> "masukkan data yang ingin di update dan idnya"')
+        console.log('article delete    ->  delete<space> "masukkan id article"')
         console.log('========================================================')
     }
 }
