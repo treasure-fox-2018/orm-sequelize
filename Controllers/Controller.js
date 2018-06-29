5acc4a1453f081ce3c0fb9b5b1f708150f394d88
dawimuhammad <muhamadhaddawi@gmail.com>
2018-06-29 11:42:17 +0700

    Release 1 - CRUD feature for all table

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -19,9 +19,20 @@ class Controller {
               .then(author => console.log(`Author ${author.first_name} ${author.last_name} successfully created!`))
               .catch(err => console.log(err))
         } else if (table === 'tag') {
-
+            Tag.create({
+                name: inputs[0]
+            })
+            .then(tag => console.log(`Tag ${tag.name} has successfully created`))
+            .catch(err => console.log(err))
         } else if (table === 'article') {
-
+            Article.create({
+                title : inputs[0],
+                body : inputs[1],
+                AuthorId : inputs[2],
+                TagId : inputs[3],
+            })
+              .then(createdArticle => console.log(createdArticle.get({raw: true})))
+              .catch(err => console.log(err))
         }
     }
 
@@ -30,26 +41,35 @@ class Controller {
             let authorId = inputs[0]
             Author.findOne({where: {id: authorId}})
             .then(author =>
-                console.log(`Author name : ${author.first_name} ${author.last_name}\n
-                Religion : ${author.religion}\n
-                Gender : ${author.gender}\n
-                Age : ${author.age}`))
+                console.log(author.get({plain:true})))
             .catch(err => console.log(err))
         } else if (table === 'tag') {
-
+            let tagId = inputs[0]
+            Tag.findOne({where: {id: tagId}})
+            .then(tag =>
+                console.log(tag.get({plain:true})))
+            .catch(err => console.log(err))
         } else if (table === 'article') {
-
+            let articleId = inputs[0]
+            Article.findOne({where: {id: articleId}})
+            .then(article => console.log(article.get({plain:true})))
+            .catch(err => console.log(err))
         }
     }
 
     static readAllTable(table, inputs) {
         if (table === 'author') {
             Author.findAll()
-            .then(authors => console.log(authors))
+            .then(allAuthors => console.log(allAuthors))
+            .catch(err => console.log(err))
         } else if (table === 'tag') {
-
+            Tag.findAll()
+            .then(allTags => console.log(allTags))
+            .catch(err => console.log(err))
         } else if (table === 'article') {
-
+            Article.findAll()
+            .then(allArticles => console.log(allArticles))
+            .catch(err => console.log(err))
         }
     }
 
@@ -63,12 +83,23 @@ class Controller {
             let authorAge = inputs[5]
             Author.update({first_name : authorFirstName, last_name: authorLastName, religion: authorReligion, gender:authorGender, age:authorAge},
                           {where: {id: authorId}})
-            .then(authorUpdated => console.log(authorUpdated))
+            .then(authorUpdated => console.log(authorUpdated.get({plain:true})))
             .catch(err => console.log(err))
         } else if (table === 'tag') {
-
+            let tagId = inputs[0]
+            let tagSetName = inputs[1]
+            Tag.update({name: tagSetName}, {where: {id:tagId}})
+            .then(updatedTag => console.log(updatedTag))
+            .catch(err => console.log(err))
         } else if (table === 'article') {
+            let articleId = inputs[0]
+            let articleTitle = inputs[1]
+            let articleBody = inputs[2]
 
+            Article.update({title : articleTitle, body : articleTitle},
+                           {where: {id : articleId}})
+            .then(console.log(`successfully updated!`))
+            .catch(err => console.log(err))
         }
     }
 
@@ -79,9 +110,15 @@ class Controller {
             .then(deletedAuthor => `Author ${deletedAuthor.first_name} ${deletedAuthor.last_name} successfully created!`)
             .catch(err => console.log(err))
         } else if (table === 'tag') {
-
+            let tagId = inputs[0]
+            Tag.destroy({where: {id:tagId}})
+            .then(deletedTag => `Tag ${deletedTag.name} has successfully deleted`)
+            .catch(err => console.log(err))
         } else if (table === 'article') {
-
+            let articleId = inputs[0]
+            Article.destroy({where: {id: articleId}})
+            .then(deletedArticle => `Artcile with title : ${deletedArticle.title} and id : ${deletedArticle.id} has successfully deleted!`)
+            .catch(err => console.log(err))
         }
     }
 
